0896d0f3d754f87bdf263a1ddcc5f11f9a8a0a3f
Tanmay Sutar <58669933+Tanny2109@users.noreply.github.com>
2022-04-09 03:19:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
-email = \"email@domain\"\n\
+email = \"tanmayss2109@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-
 " > ~/.streamlit/config.tomls
